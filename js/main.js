28b5a57ecdda3289a3c17918cec2f3e167b8c09f
Dima Zhadan <dimka.jadanxaxa@gmail.com>
2022-09-30 16:32:09 +0300

    create sections: description and offer, also create normal tabs in offer section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Hamburger menu
 const btnHamburger = document.querySelector('.header__body-hamburger'),
    menuMobile = document.querySelector('.header__mobaile'),
    headerBody = document.querySelector('.header__body');
@@ -8,5 +9,25 @@ btnHamburger.addEventListener('click', function () {
    menuMobile.classList.toggle('active');
    btnHamburger.classList.toggle('active-hamburger');
 });
+
+//tabs
+
+function openTab(evt, tabName) {
+   let i, tabcontent, tablinks;
+
+   tabcontent = document.getElementsByClassName('offer__section');
+   for (i = 0; i < tabcontent.length; i++) {
+      // tabcontent[i].className += '';
+   }
+
+   tablinks = document.getElementsByClassName('offer__item');
+   for (i = 0; i < tabcontent.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace('offer__item-active');
+      tabcontent[i].className = tabcontent[i].className.replace('offer__section-active');
+   }
+
+   document.getElementById(tabName).className += ' offer__section-active';
+   evt.currentTarget.className += ' offer__item-active';
+}
 
 
